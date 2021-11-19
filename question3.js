6d15cc0af1678b21d983a0ae64bd68288299e103
Jaline Foo <jaline.foo@foodpanda.com>
2021-11-19 14:29:36 +0800

    updated question3 ans

--- question3.js
+++ question3.js
@@ -6,39 +6,26 @@ for (let j = 0; j <mockData.length; j++) {
     let fullName;
     let atLeastOne;
 
-    const numOfStocks = mockData[j].portfolio.length; //displays num of obj in each portfolio array
+    const numOfStocks = mockData[j].portfolio.length; 
 
-            // to prevent "undefined" in final console log
-            // if (numOfStocks >= 1) {
-            //     atLeastOne = mockData[j].firstName + " " + mockData[j].lastName;
-            //     //console.log(atLeastOne);
-            // }
+    // skips over people with no stock
+    // The continue statement "jumps over" one iteration in the loop
+     if (!numOfStocks) {
+        continue;
+     } 
+     else if (numOfStocks >= 1) {
+        atLeastOne = mockData[j].portfolio.length; 
+    }
 
-        for(let k = 0; k<mockData[j].portfolio.length; k++){
+        for(let k = 0; k < atLeastOne; k++){
             const portPosition = mockData[j].portfolio[k].position;
+            //console.log(portPosition);
 
-            if ( numOfStocks >= 1 && portPosition > 200) {
-                fullName = mockData[j].firstName + " " + mockData[j].lastName;
+            if (portPosition > 200) {
+            fullName = mockData[j].firstName + " " + mockData[j].lastName;
             }
         }
 
-console.log("Full name of person with portfolio position of more than 200 : " + fullName);
-}
-
-
-/* BEFORE
-// issue - displays duplicated names, if person A has 3 stocks of position more than 200, displayed three times
-
-for (let j = 0; j <mockData.length; j++) {
-    for(let k = 0; k<mockData[j].portfolio.length; k++){
-        const portPosition = mockData[j].portfolio[k].position;
-
-        if (portPosition > 200) {
-       const fullName = mockData[j].firstName + " " + mockData[j].lastName;
-       console.log("Full name of person with portfolio position of more than 200 : " + fullName);
-       }
-
+    console.log("Full name of person with portfolio position of more than 200 : " + fullName);
     }
-}
 
-*/
